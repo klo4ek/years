0e62a5c7d863fecf34596c1bf44cf8ca446f2880
KontiVL <kokajama10@gmail.com>
2022-10-16 22:18:13 +0700

    fun main(args: Array<String>) {         val n = 843         val x = n / 100         val y = n % 10         val z = n / 10         val zS = z % 10         val nS = x + y + zS         println("Result=$nS") }

---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MainKtTest {
+
+    @Test
+    fun main() {
+    }
+
+    @Test
+    fun getDate() {
+    }
+
+    @Test
+    fun setDate() {
+    }
+}
